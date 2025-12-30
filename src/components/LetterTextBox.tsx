8fc9459b9aa71dff5e260fdc3831838ca7537db6
yerin-lab <chayerin_@naver.com>
2025-12-30 17:18:29 +0900

    [#2] chore : test 환경 구축 (router, test-page)

---
+++ src/components/LetterTextBox.tsx
@@ -0,0 +1,5 @@
+const LetterTextBox = () => {
+  return <h1>Time to write a letter!</h1>;
+};
+
+export default LetterTextBox;
---
+++ src/pages/TestPage.tsx
@@ -0,0 +1,11 @@
+import LetterTextBox from '@/components/LetterTextBox';
+
+const TestPage = () => {
+  return (
+    <div>
+      <LetterTextBox />
+    </div>
+  );
+};
+
+export default TestPage;
