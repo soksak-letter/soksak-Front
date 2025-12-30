efd4867c0d2a7b6d6e838c090a5fc27468a09cdc
yerin-lab <chayerin_@naver.com>
2025-12-30 21:56:36 +0900

    [#2] style : 높이, 너비 맞추기

--- src/components/LetterTextBox.tsx
+++ src/components/LetterTextBox.tsx
@@ -1,12 +1,12 @@
 const LetterTextBox = () => {
   return (
-    <div className='relative flex flex-col w-full max-w-xl mx-auto'>
-      <div className='rounded-t-md bg-[#EFEFEF] px-4 py-3 text-xl font-medium text-[#8C8C8C] shadow-sm'>
+    <div className='relative flex flex-col w-full items-center justify-center max-w-xl mx-auto'>
+      <div className='min-h-[49px] w-[343px] resize-none rounded-t-md bg-[#EFEFEF] px-4 py-3 text-xl font-medium text-[#8C8C8C] shadow-sm'>
         제목
       </div>
 
       <textarea
-        className='min-h-[350px] resize-none rounded-b-md border border-none
+        className='min-h-[350px] w-[343px] resize-none rounded-b-md border border-none
                    bg-white px-4 py-3 text-base text-gray-800 shadow-sm
                    placeholder:text-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-300'
         placeholder={`오늘의 질문을 읽고 
