fb3e194451344d61654a5fdcdd2b7a09b51bdfc1
yerin-lab <chayerin_@naver.com>
2025-12-30 21:33:01 +0900

    [#2] feature : 편지지 text box 제작

--- src/components/LetterTextBox.tsx
+++ src/components/LetterTextBox.tsx
@@ -1,5 +1,21 @@
 const LetterTextBox = () => {
-  return <h1>Time to write a letter!</h1>;
+  return (
+    <div className='relative flex flex-col w-full max-w-xl mx-auto'>
+      <div className='rounded-t-md bg-[#EFEFEF] px-4 py-3 text-xl font-medium text-[#8C8C8C] shadow-sm'>
+        제목
+      </div>
+
+      <textarea
+        className='min-h-[350px] resize-none rounded-b-md border border-none
+                   bg-white px-4 py-3 text-base text-gray-800 shadow-sm
+                   placeholder:text-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-300'
+        placeholder={`오늘의 질문을 읽고 
+얘기해보고 싶은 내용이나 주제가 있나요?
+
+자유롭게 작성해보세요.`}
+      />
+    </div>
+  );
 };
 
 export default LetterTextBox;
