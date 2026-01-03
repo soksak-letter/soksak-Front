d9912d00e47637d7e5d07637047cb1720234e50d
jeniferkim <jeniferkim1673@naver.com>
2026-01-03 23:28:07 +0900

    [#8] chore: 한림고딕체 주석 처리 (디자이너와 상의 필요)

--- tailwind.config.js
+++ tailwind.config.js
@@ -61,7 +61,7 @@ export default {
       },
 
       fontFamily: {
-        hallym: 'var(--font-hallym)',
+        // hallym: 'var(--font-hallym)',
         pretendard: 'var(--font-pretendard)',
       },
 
