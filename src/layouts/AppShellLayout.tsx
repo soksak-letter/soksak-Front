df44e5aa15322f04fb6735b566d3329b481c58fd
jeniferkim <jeniferkim1673@naver.com>
2026-01-04 17:37:47 +0900

    [#11] fix: 앱쉘에서 헤더, 탭바 삭제

--- src/layouts/AppShellLayout.tsx
+++ src/layouts/AppShellLayout.tsx
@@ -1,15 +1,11 @@
-import TabBar from '@/components/TabBar';
-import BackHeader from '@/components/common/headers/BackHeader';
 import { Outlet } from 'react-router-dom';
 
 export default function AppShellLayout() {
   return (
-    <div className="min-h-dvh bg-[#f5f5f5]">
-      <div className="mx-auto min-h-dvh w-[375px] bg-white">
-        <BackHeader title='친구찾기' rightElement='추가' />
+    <div className='min-h-dvh bg-[#f5f5f5]'>
+      <div className='mx-auto min-h-dvh w-[375px] bg-white'>
         {/* <main className="flex-1 overflow-y-auto"></main> */}
         <Outlet />
-        <TabBar />
       </div>
     </div>
   );
