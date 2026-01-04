ae4405fa4829e255fc91a072ca26ef2acacae446
jeniferkim <jeniferkim1673@naver.com>
2026-01-04 17:44:57 +0900

    [#11] chore: css 변수 수정 및 주석 추가

--- src/layouts/AppShellLayout.tsx
+++ src/layouts/AppShellLayout.tsx
@@ -1,9 +1,11 @@
+// 모바일 너비 375px 프레임만
+
 import { Outlet } from 'react-router-dom';
 
 export default function AppShellLayout() {
   return (
     <div className='min-h-dvh bg-[#f5f5f5]'>
-      <div className='mx-auto min-h-dvh w-[375px] bg-white'>
+      <div className='mx-auto min-h-dvh w-[375px] bg-[var(--color-bg-primary)]'>
         {/* <main className="flex-1 overflow-y-auto"></main> */}
         <Outlet />
       </div>
