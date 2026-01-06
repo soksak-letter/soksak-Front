8fc58eca810e05f8c03040a161012ff2bc2accf3
yerin-lab <chayerin_@naver.com>
2026-01-07 02:40:26 +0900

    [#22] style : 오늘의 질문 말풍선 영역 고정 처리

--- src/pages/letter/OtherDraftPage.tsx
+++ src/pages/letter/OtherDraftPage.tsx
@@ -20,7 +20,7 @@ const OtherDraftPage = () => {
   return (
     <div className='flex flex-col'>
       <BackHeader title={`${nickname}에게 보내는 편지`} rightElement={<button>꾸미기</button>} />
-      <div className='flex flex-col items-start p-5 gap-3'>
+      <div className='flex flex-col items-start p-5 -mt-3 gap-3'>
         <div className='text-[18px] font-semibold leading-[160%]'>
           <span className='text-black'>우리에게 남은 편지 횟수는 </span>
           <span className='text-[var(--color-primary-500)]'>{letterLeft}회</span>
@@ -37,7 +37,9 @@ const OtherDraftPage = () => {
             오늘의 질문이 궁금하다면?
           </span>
         </div>
-        {isClicked && (
+        <div
+          className={`flex flex-col items-start transition-opacity duration-200 ${isClicked ? 'opacity-100' : 'opacity-0'}`}
+        >
           <div className='flex flex-col items-start -ml-2 -mt-1'>
             {/* 꼬리 */}
             <div className='ml-[9px] h-0 w-0 border-x-[6px] border-x-transparent border-b-[8px] border-b-[#E5E6E6]' />
@@ -49,18 +51,18 @@ const OtherDraftPage = () => {
               {dailyQuestion}
             </div>
           </div>
-        )}
+        </div>
       </div>
       <div className='px-4'>
         <LetterTextBox value={letter} onChange={setLetter} className='w-[343px] h-[394px]' />
       </div>
-      <div className='flex items-center justify-end p-5 gap-2'>
+      <div className='flex items-center justify-end p-5 -mt-3 gap-2'>
         <span className='text-[var(--color-text-normal)] text-[14px] font-medium'>
           오늘 하루 동안 편지 공개하기
         </span>
         <ToggleSwitch checked={isPublic} onCheckedChange={setIsPublic} />
       </div>
-      <p className='flex p-5 text-[12px] text-[var(--color-text-assistive)] leading-[160%] font-medium'>
+      <p className='flex p-5 -mt-3 text-[12px] text-[var(--color-text-assistive)] leading-[160%] font-medium'>
         비방의 언어가 담기면 자동으로 필터링 돼요.
         <br />
         상대방에 대한 존중이 담긴 언어로 따뜻한 편지를 전달해주세요.
