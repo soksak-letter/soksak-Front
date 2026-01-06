755b49becf098208e10d252ad35911dd0465ebb5
yerin-lab <chayerin_@naver.com>
2026-01-07 02:10:08 +0900

    [#22] feature : 목데이터(dailyQuestion) 생성, 질문 보이기

--- src/pages/letter/OtherDraftPage.tsx
+++ src/pages/letter/OtherDraftPage.tsx
@@ -1,7 +1,8 @@
 import BackHeader from '@/components/common/headers/BackHeader';
 import ToggleSwitch from '@/components/common/ToggleSwitch';
 import LetterTextBox from '@/components/LetterTextBox';
-import { useMemo, useRef, useState } from 'react';
+import { useState } from 'react';
+import Question from '@/assets/icons/Question.svg?react';
 
 const OtherDraftPage = () => {
   const [letter, setLetter] = useState({
@@ -9,10 +10,12 @@ const OtherDraftPage = () => {
     content: '',
   });
   const [isPublic, setIsPublic] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
 
   // mock data
   const nickname = '파란수박';
   const letterLeft = '4';
+  const dailyQuestion = '당신의 인생에 큰 영감을 주는 사람은 누구인가요?';
 
   return (
     <div className='flex flex-col'>
@@ -22,6 +25,31 @@ const OtherDraftPage = () => {
           <span className='text-black'>우리에게 남은 편지 횟수는 </span>
           <span className='text-[var(--color-primary-500)]'>{letterLeft}회</span>
         </div>
+        <div className='flex items-center gap-2'>
+          <button
+            type='button'
+            className='flex items-center w-[14px] h-[14px]'
+            onClick={() => setIsClicked((v) => !v)}
+          >
+            <Question />
+          </button>
+          <span className='text-[12px] text-[var(--color-text-assistive)]'>
+            오늘의 질문이 궁금하다면?
+          </span>
+        </div>
+        {isClicked && (
+          <div className='flex flex-col items-start -ml-2 -mt-1'>
+            {/* 꼬리 */}
+            <div className='ml-[9px] h-0 w-0 border-x-[6px] border-x-transparent border-b-[8px] border-b-[#E5E6E6]' />
+            {/* 본문 */}
+            <div
+              className='max-w-[340px] rounded-[10px] bg-[#E5E6E6] px-3 py-2
+                      text-[12px] font-medium leading-[160%] text-[var(--color-text-normal)] shadow-sm'
+            >
+              {dailyQuestion}
+            </div>
+          </div>
+        )}
       </div>
       <div className='px-4'>
         <LetterTextBox value={letter} onChange={setLetter} className='w-[343px] h-[394px]' />
