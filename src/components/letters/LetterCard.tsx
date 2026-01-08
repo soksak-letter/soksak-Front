88e039abaf9ed7201def9b34cbd253800e25daab
yerin-lab <chayerin_@naver.com>
2026-01-08 12:48:55 +0900

    [#32] refactor : 스타일 객체 컴포넌트 외부로 이동 - 렌더링 개선

--- src/components/letters/LetterCard.tsx
+++ src/components/letters/LetterCard.tsx
@@ -4,22 +4,22 @@ import type { CSSProperties } from 'react';
 // 임의로 만든 letter
 // 디자인 확정 후 수정 필요
 
-const LetterCard = () => {
-  const paperStyle: CSSProperties = {
-    // 종이 배경색
-    backgroundColor: '#E6E2F2',
-    // 줄(가로) 반복
-    backgroundImage: `repeating-linear-gradient(
-      to bottom,
-      rgba(0, 0, 0, 0.25) 0px,
-      rgba(0, 0, 0, 0.25) 1px,
-      transparent 1px,
-      transparent 14px
-    )`,
-    // 위에서부터 줄 시작 위치(위쪽 여백 느낌)
-    backgroundPosition: '0 18px',
-  };
+const paperStyle: CSSProperties = {
+  // 종이 배경색
+  backgroundColor: '#E6E2F2',
+  // 줄(가로) 반복
+  backgroundImage: `repeating-linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25) 0px,
+    rgba(0, 0, 0, 0.25) 1px,
+    transparent 1px,
+    transparent 14px
+  )`,
+  // 위에서부터 줄 시작 위치(위쪽 여백 느낌)
+  backgroundPosition: '0 18px',
+};
 
+const LetterCard = () => {
   return (
     <div className='flex justify-center'>
       <div
