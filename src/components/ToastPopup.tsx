b984c428237137a1d585714be572dcef5faf6173
yerin-lab <chayerin_@naver.com>
2026-01-17 23:30:25 +0900

    [#48] chore : TODO 추가 : 스타일링 문법 수정 필요

--- src/components/ToastPopup.tsx
+++ src/components/ToastPopup.tsx
@@ -13,12 +13,14 @@ const ICON_MAP: Record<ToastStatus, ReactNode> = {
   success: (
     <Icon
       icon='lets-icons:check-fill'
+      // TODO : var 사용 하지 않기
       className='text-[var(--color-primary-500)] w-[24px] h-[24px]'
     />
   ),
   error: (
     <Icon
       icon='zondicons:exclamation-solid'
+      // TODO : var 사용 하지 않기
       className='text-[var(--color-primary-500)] w-[24px] h-[24px]'
     />
   ),
@@ -26,6 +28,7 @@ const ICON_MAP: Record<ToastStatus, ReactNode> = {
 
 const ToastPopup = ({ status, message }: ToastPopupProps) => {
   return (
+    // TODO : var 사용 하지 않기
     <div className='flex items-center w-[343px] h-[56px] gap-2 px-4 py-3 rounded-lg shadow-md bg-[var(--color-primary-100)]'>
       {ICON_MAP[status]}
       <span className='text-sm font-medium'>{message}</span>
