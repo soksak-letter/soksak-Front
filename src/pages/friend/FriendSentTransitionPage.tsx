c3ee8f3b3fc946df3b75b635071146b45548201c
yerin-lab <chayerin_@naver.com>
2026-01-19 01:06:57 +0900

    [#55] style : 디자인 완성

--- src/pages/friend/FriendSentTransitionPage.tsx
+++ src/pages/friend/FriendSentTransitionPage.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/common/Button';
 import LetterEndedEnvelope from '@/assets/icons/LetterEndedEnvelope.svg?react';
+import { Link } from 'react-router-dom';
 
 export default function FriendSentTransitionPage() {
   // mock data
@@ -7,23 +8,35 @@ export default function FriendSentTransitionPage() {
   const receiver = '파란수박';
 
   return (
-    <div>
-      <header>
-        <p>
-          {receiver}님과 10회의 대화를
+    <div className='min-h-dvh px-6 pt-14 pb-28'>
+      {/* Header */}
+      <header className='flex flex-col justify-start gap-2'>
+        <p className='ty-body1 leading-tight'>
+          {receiver}님과 <span className='text-(--color-primary-500)'>10회</span>의 대화를
           <br />
           모두 나누었어요.
         </p>
-        <p>인연을 이어가시려면 친구로 추가해주세요.</p>
+        <p className='ty-body3'>인연을 이어가시려면 친구로 추가해주세요.</p>
       </header>
-      <div>
-        <link>우리가 나눴던 대화 다시보기</link>
-      </div>
-      <div>
-        <LetterEndedEnvelope />
-        <Button>친구 신청</Button>
-        <Button>후기 남기기</Button>
-      </div>
+
+      {/* Envelope + Link */}
+      <section className='mt-15 flex flex-col items-center'>
+        <LetterEndedEnvelope className='block' />
+        <Link
+          to={`/friend/post`}
+          className='ty-body5 text-(--color-text-assistive) mt-3 underline underline-offset-4'
+        >
+          우리가 나눴던 대화 다시보기
+        </Link>
+      </section>
+
+      {/* Buttons */}
+      <section className='mt-15 flex flex-col items-center gap-3'>
+        <Button className='w-[343px]'>친구 신청</Button>
+        <Button className='w-[343px]' color='white'>
+          후기 남기기
+        </Button>
+      </section>
     </div>
   );
 }
