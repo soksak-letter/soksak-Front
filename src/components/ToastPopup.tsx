1201597502f5dc17c9fd13be23d2e4c593a56180
yerin-lab <chayerin_@naver.com>
2026-01-19 12:57:48 +0900

    [#48] chore : 스타일링 컨벤션 통일 - var 사용하지 않기

--- src/components/ToastPopup.tsx
+++ src/components/ToastPopup.tsx
@@ -1,5 +1,4 @@
 import type { ReactNode } from 'react';
-
 import { Icon } from '@iconify/react';
 
 export type ToastStatus = 'success' | 'error';
@@ -13,26 +12,20 @@ export type ToastPopupProps = {
 
 const ICON_MAP: Record<ToastStatus, ReactNode> = {
   success: (
-    <Icon
-      icon='lets-icons:check-fill'
-      // TODO : var 사용 하지 않기
-      className='text-[var(--color-primary-500)] w-[18px] h-[18px]'
-    />
+    <Icon icon='lets-icons:check-fill' className='text-(--color-primary-500) w-[18px] h-[18px]' />
   ),
   error: (
     <Icon
       icon='zondicons:exclamation-solid'
-      // TODO : var 사용 하지 않기
-      className='text-[var(--color-primary-500)] w-[18px] h-[18px]'
+      className='text-(--color-primary-500) w-[18px] h-[18px]'
     />
   ),
 };
 
 const ToastPopup = ({ status, message, visible, onClose }: ToastPopupProps) => {
   return (
-    // TODO : var 사용 하지 않기
     <div
-      className={`flex items-center w-[343px] h-[56px] gap-3 px-4 py-3 rounded-[32px] shadow-md bg-[var(--color-primary-100)] transition-all duration-300 ease-out
+      className={`flex items-center w-[343px] h-[56px] gap-3 px-4 py-3 rounded-[32px] shadow-md bg-(--color-primary-100) transition-all duration-300 ease-out
         ${visible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-2'}
       `}
       onClick={onClose}
