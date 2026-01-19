eb2c78967739f937ec97fd9d059b41a9a7a6b505
ysujii <suji072443@gmail.com>
2026-01-19 19:09:50 +0900

    [#67] feat:api 기본 설정

---
+++ src/api/auth.ts
@@ -0,0 +1,13 @@
+import type { LoginRequest, SignUpRequest, SignUpResponse } from "@/types/dto/response";
+import { axiosInstance } from "./api";
+
+
+// 회원가입 API 함수
+export const signUp = async (data: SignUpRequest): Promise<SignUpResponse> => {
+  // 제네릭 <SignUpResponse>를 사용하여 응답 타입을 명시합니다.
+  const response = await axiosInstance.post<SignUpResponse>('/auth/signup', data);
+  return response.data; // axios의 data 객체만 반환
+};
+
+// 로그인 API 함수
+export const login = async (data: LoginRequest) => { ... }
---
+++ src/types/dto/auth.ts
@@ -0,0 +1,9 @@
+import { CommonResponse } from './response.tsx';
+
+// Auth API DTO
+
+// Refresh Token Response
+export type RefreshTokenResponse = CommonResponse<null>;
+
+// Logout Response
+export type LogoutResponse = CommonResponse<null>;
