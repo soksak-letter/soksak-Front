8004b8e4e4d35c1fa4638805db92d7b0464f6d7a
yerin-lab <chayerin_@naver.com>
2026-02-01 22:42:47 +0900

    [#110] chore : 안 쓰는 라우터 삭제

--- src/Router.tsx
+++ src/Router.tsx
@@ -85,8 +85,6 @@ const router = createBrowserRouter([
                   { path: 'inbox-self', element: <LetterInboxSelfPage /> },
                 ],
               },
-              // { path: 'letter/inbox-other', element: <LetterInboxOtherPage /> }, // letter/inbox-self에서도 탭바 활성화 되도록
-              // { path: 'letter/inbox-self', element: <LetterInboxSelfPage /> },
               // { path: 'letter/10-end', element: <LetterTenEndPage /> },
               { path: 'letter/other-stop', element: <LetterOtherStopPage /> },
               { path: 'friend/request', element: <FriendRequestPage /> },
@@ -147,7 +145,6 @@ const router = createBrowserRouter([
                   { path: 'draft', element: <LetterDraftRoute /> },
                   { path: 'decorate', element: <LetterDecoPage /> },
                   { path: 'sending', element: <LetterSendingPage /> },
-                  // { path: 'sent-transition', element: <LetterSendingPage /> }, TODO: 예디랑 논의 필요
                 ],
               },
               { path: 'letter/thread/:threadId', element: <LetterPostOtherPage /> },
@@ -155,7 +152,6 @@ const router = createBrowserRouter([
               { path: 'letter/reply/:threadId/:letterId', element: <LetterReplyPage /> },
               { path: 'letter/report', element: <LetterReportPage /> },
               { path: 'letter/review/:letterId', element: <LetterReviewPage /> },
-              // { path: 'letter/post-self', element: <LetterPostSelfPage /> }, // TODO : 미사용 라우터 삭제
               { path: 'letter/post-self/:letterId', element: <LetterPostSelfPage /> },
               { path: 'letter/loading', element: <LoadingPage /> },
               // 기존 코드 충돌 방지를 위한 코드(레거시). 추후 삭제
@@ -237,26 +233,6 @@ const router = createBrowserRouter([
 
           // 404 처리
           { path: '*', element: <NotFoundPage /> },
-
-          // 기존 코드. 주석처리
-          // {
-          // path: 'letter/anon-draft',
-          // element: <AnonDraftPage />,
-          // },
-          // {
-          // path: 'letter/other-draft',
-          // element: <OtherDraftPage />,
-          // },
-          // {
-          // path: 'letter/self-draft',
-          // element: <SelfDraftPage />,
-          // },
-          // {
-          // path: 'letter/:mode-decorate',
-          // element: <LetterDecoPage />,
-          // },
-
-          // { path: 'letter/report', element: <LetterReportPage /> },
         ],
       },
     ],
