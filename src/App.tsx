6d64edfe882d7e941788bce23082e73766d60588
jeniferkim <jeniferkim1673@naver.com>
2026-02-03 19:28:13 +0900

    [#122] feat: 전역 ActivityTracker 마운트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import ModalRoot from './components/modal/ModalRoot';
+import ActivityTracker from './components/ActivityTracker';
 
 function App() {
   return (
     <>
+      <ActivityTracker />
       <ModalRoot />
       <Outlet />
     </>
