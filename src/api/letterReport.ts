f3033c6016d1930cd96ace3fa820596ae3c08d89
ysujii <suji072443@gmail.com>
2026-02-04 01:39:23 +0900

    [#121] chore:letterReport API 로직에서 주석 처리되지 않은 부분 수정

--- src/api/letterReport.ts
+++ src/api/letterReport.ts
@@ -3,7 +3,7 @@ import { axiosInstance } from './axios';
 
 /**
  *신고하기 API
- @param body letterId,reason[]
+ * @param body 신고 요청 데이터 (편지 ID와 신고 사유 배열)
  * Post /reports
  */
 export const postLetterReport = async (body: LetterReportRequest) => {
