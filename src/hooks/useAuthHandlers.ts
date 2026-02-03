8f01f02620430dbb22d07f07fba6e22969c09cfc
ysujii <suji072443@gmail.com>
2026-02-04 00:55:03 +0900

    [#124] feat:로그아웃을 따로 핸들러로 빼 로직 구현

---
+++ src/hooks/useAuthHandlers.ts
@@ -0,0 +1,42 @@
+import { useNavigate } from 'react-router-dom';
+import { useAuthStore } from '@/stores/useAuthStore';
+import { postLogout } from '@/api/auth'; //TODO: 회원탈퇴 API
+export const useAuthHandlers = () => {
+  const navigate = useNavigate();
+  const logout = useAuthStore((state) => state.logout);
+
+  // 1. 로그아웃 핸들러
+  const handleLogout = async () => {
+    try {
+      await postLogout(); // 서버에 "나 간다" 알림
+    } catch (error) {
+      console.error('로그아웃 실패(토큰만료 등):', error);
+    } finally {
+      // 성공하든 실패하든 클라이언트는 무조건 로그아웃 처리
+      logout();
+      navigate('/splash', { replace: true });
+    }
+  };
+
+  {
+    /*// 2. 회원탈퇴 핸들러
+  const handleWithdraw = async () => {
+    try {
+      // 회원탈퇴 API 호출
+      // await postWithdraw(); 
+      
+      console.log('회원탈퇴 성공');
+      
+      // 탈퇴 후에도 로그아웃 처리와 동일하게 청소 필요
+      logout();
+      navigate('/splash', { replace: true });
+    } catch (error) {
+      console.error('회원탈퇴 실패:', error);
+      // 에러 시 토스트 메시지 등을 띄울 수 있음
+      alert('회원탈퇴 처리에 실패했습니다. 잠시 후 다시 시도해주세요.');
+    }
+  };
+*/
+  }
+  return { handleLogout };
+};
