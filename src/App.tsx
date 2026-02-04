16aa30ab46e11e33ea0b7bdf2a3b3314258b35f0
jeniferkim <jeniferkim1673@naver.com>
2026-02-04 17:23:04 +0900

    [#122] refactor: hasToken을 전역 store로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import ModalRoot from './components/modal/ModalRoot';
 import ActivityTracker from './components/ActivityTracker';
+import { useAuthStore } from './stores/useAuthStore';
 
 function App() {
-  const hasToken = Boolean(localStorage.getItem('token'));
+  const hasToken = useAuthStore((s) => s.hasToken);
 
   return (
     <>
