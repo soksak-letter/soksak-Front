0f417b581807b54ee334bb49aabd87030a45a3da
yerin-lab <chayerin_@naver.com>
2026-02-06 01:52:15 +0900

    [#112] fix : 좋아요 API 호출 함수에서 성공시/실패시 예외 처리 강화

--- src/api/like.ts
+++ src/api/like.ts
@@ -6,12 +6,12 @@ export async function postLetterLike(letterId: number): Promise<CreateLikeSucces
 
   const data = res.data as CreateLikeResponse;
 
-  if (data.resultType !== 'SUCCESS') {
+  if (data.resultType !== 'SUCCESS' || !data.success) {
     throw (
       data.error ?? {
         errorCode: 'CREATE_LIKE_FAIL',
         reason: '좋아요 누르기에 실패했어요. 잠시 후 다시 시도해주세요.',
-        data: null,
+        data: {},
       }
     );
   }
@@ -24,12 +24,12 @@ export async function deleteLetterLike(letterId: number): Promise<CreateLikeSucc
 
   const data = res.data as CreateLikeResponse;
 
-  if (data.resultType !== 'SUCCESS') {
+  if (data.resultType !== 'SUCCESS' || !data.success) {
     throw (
       data.error ?? {
         errorCode: 'DELETE_LIKE_FAIL',
         reason: '좋아요 삭제에 실패했어요. 잠시 후 다시 시도해주세요.',
-        data: null,
+        data: {},
       }
     );
   }
