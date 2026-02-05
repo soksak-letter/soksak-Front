25fd9163524b9e85c2580bdcc87f71daf54d9856
ysujii <suji072443@gmail.com>
2026-02-05 23:53:04 +0900

    [#135] feat:비밀번호 변경(설정 페이지) API 로직 구현 및 비밀번호 초기화(비밀번호 재설정 페이지) 주석 수정

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,8 @@
 import type {
   AgreementsRequest,
   AgreementsResponse,
+  ChangePasswordRequest,
+  ChangePasswordResponse,
   EmailExistsRequest,
   EmailExistsResponse,
   LogoutResponse,
@@ -72,6 +74,18 @@ export const deleteWithdraw = async () => {
   const { data } = await axiosInstance.delete<WithdrawResponse>('/users');
   return data;
 };
+
+/**
+ * 바말번호 재설정ㄴ API
+ * PATCH /auth/change-password
+ */
+export const changePassword = async (
+  data: ChangePasswordRequest,
+): Promise<ChangePasswordResponse> => {
+  const response = await axiosInstance.patch<ChangePasswordResponse>('/auth/change-password', data);
+  return response.data;
+};
+
 /**
  * 소셜 로그인 (인가 코드 -> 토큰 교환)
  * @param provider 'google' | 'kakao' | 'naver'
