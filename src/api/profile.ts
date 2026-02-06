5130b49852cad06c5a711e74b9c40ad00d281812
ysujii <suji072443@gmail.com>
2026-02-06 19:12:27 +0900

    [#145] feat:프로필 조회 API 함수 구현

---
+++ src/api/profile.ts
@@ -0,0 +1,11 @@
+import type { MyProfileResponse } from '@/types/dto/profile';
+import { axiosInstance } from './axios';
+
+/**
+ * 사용자의 프로필 정보를 조회
+ * GET /users/me/profile
+ */
+export const onboardingApi = async (): Promise<MyProfileResponse> => {
+  const { data } = await axiosInstance.get<MyProfileResponse>('/users/me/profile');
+  return data;
+};
