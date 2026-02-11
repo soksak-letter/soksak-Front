91c212fff197715b0b2f60b1bccf1f4d2e7a07b3
yerin-lab <chayerin_@naver.com>
2026-02-11 16:04:25 +0900

    [#153] fix : other, anon 상태에서 편지 발송시 receiverUserId가 null값으로 전달되는 이슈 해결 중

--- src/pages/onboarding/OnboardingLetterGuidePage.tsx
+++ src/pages/onboarding/OnboardingLetterGuidePage.tsx
@@ -35,7 +35,7 @@ export default function OnboardingLetterGuidePage() {
   const mintPaperId = 1; //  mint paper 선택
   const PaperBg = PAPER_ASSET_MAP[mintPaperId].Preview;
 
-  const fontId = 3; // TODO: 나눔히피체가 추가되면 해당 폰트 ID로 교체 필요
+  const fontId = 4; // 나눔 바른히피체 선택
   const fontFamily = FONT_ASSET_MAP[fontId].fontFamily;
 
   useEffect(() => {
