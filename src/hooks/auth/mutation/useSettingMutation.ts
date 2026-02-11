879fb6b459b20452235d42bdeab3a2a8de1e2c98
user <user@MacBook-Pro.local>
2026-02-11 16:51:56 +0900

    [#161] refactor : 설정페이지 비밀번호 재설정 에러처리 unknown으로

--- src/hooks/auth/mutation/useSettingMutation.ts
+++ src/hooks/auth/mutation/useSettingMutation.ts
@@ -3,6 +3,16 @@ import { patchChangePassword } from '@/api/auth';
 import { useNavigate } from 'react-router-dom';
 import { useGlobalToast } from '@/components/toast/ToastProvider';
 import { ROUTES } from '@/routes/paths';
+import axios from 'axios';
+
+// 에러 응답 인터페이스 정의
+interface ApiErrorResponse {
+  error: {
+    errorCode: string;
+    reason: string;
+    data: Record<string, unknown> | Array<{ field: string; message: string }>; // 빈 객체 {} 도 포함 가능하도록
+  };
+}
 //비밀번호 변경
 export const useChangePasswordMutation = () => {
   const navigate = useNavigate();
@@ -24,10 +34,13 @@ export const useChangePasswordMutation = () => {
     onError: (error: unknown) => {
       let message = '현재 비밀번호가 일치하지 않거나 오류가 발생했습니다.';
 
-      if (error instanceof Error) {
-        const axiosError = error as any;
-        message = axiosError.response?.data?.error?.reason || error.message || message;
+      if (axios.isAxiosError<ApiErrorResponse>(error)) {
+        message = error.response?.data?.error?.reason || message;
+      } else if (error instanceof Error) {
+        // 일반적인 Error 객체인 경우
+        message = error.message;
       }
+
       showToast(message, 'error');
     },
   });
