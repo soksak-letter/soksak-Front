56adb3a5732d03e31161f0ffd518874fe403b0ac
yuninam2128 <162120020+yuninam2128@users.noreply.github.com>
2026-02-12 04:06:39 +0900

    [#162] style : 스켈레톤 ui css를 디자인토큰으로 교체

--- src/components/skeleton/DraftSkeleton.tsx
+++ src/components/skeleton/DraftSkeleton.tsx
@@ -13,27 +13,27 @@ export default function DraftSkeleton({ title = '편지 작성' }: DraftSkeleton
       {/* 헤더 (BackHeader 동일 높이) */}
       <div className='flex items-center py-2.5 px-2.5'>
         <div className='flex-1 flex justify-center'>
-          <span className='ty-title3 text-black'>{title}</span>
+          <span className='ty-title3 text-[var(--color-text-strong)]'>{title}</span>
         </div>
       </div>
       {/* 질문 영역 */}
       <div className='flex flex-col items-start p-5 -mt-3 gap-2'>
-        <div className='w-[251px] h-[50px] rounded bg-gray-200' />
-        <div className='w-[200px] h-[20px] rounded bg-gray-200' />
+        <div className='w-[251px] h-[50px] rounded bg-[var(--color-grey-100)]' />
+        <div className='w-[200px] h-[20px] rounded bg-[var(--color-grey-100)]' />
       </div>
       {/* 편지 텍스트 박스 */}
       <div className='px-4'>
-        <div className='w-[343px] h-[394px] rounded-xl bg-gray-200' />
+        <div className='w-[343px] h-[394px] rounded-xl bg-[var(--color-grey-100)]' />
       </div>
       {/* 토글 영역 */}
       <div className='flex items-center justify-end p-5 mt-8 gap-2'>
-        <div className='w-[170px] h-[16px] rounded bg-gray-200' />
+        <div className='w-[170px] h-[16px] rounded bg-[var(--color-grey-100)]' />
       </div>
       {/* 안내 텍스트 영역*/}
       <div className='flex flex-col gap-1 px-5 -mt-3'>
         <div className='flex p-5 -mt-3 ty-detailMedium text-[var(--color-text-assistive)] flex-col gap-1'>
-          <div className='w-[220px] h-[14px] rounded bg-gray-200 mb-1' />
-          <div className='w-[180px] h-[14px] rounded bg-gray-200' />
+          <div className='w-[220px] h-[14px] rounded bg-[var(--color-grey-100)] mb-1' />
+          <div className='w-[180px] h-[14px] rounded bg-[var(--color-grey-100)]' />
         </div>
       </div>
     </div>
