f4401dddb15f5620387473458e762a23a73dd6b6
yerin-lab <chayerin_@naver.com>
2026-02-12 14:55:09 +0900

    [#186] chore : tailwind 문법 오류 해결

--- src/components/BottomSheet/letterStyle/Previews.tsx
+++ src/components/BottomSheet/letterStyle/Previews.tsx
@@ -1,11 +1,10 @@
 export function PaperPreview({ src, name }: { src: string; name: string }) {
   return (
-    <div className='w-full h-full overflow-hidden' aria-label={name}>
-      {/* name 사용하실 거면 추후에 수정해주세요 */}
+    <div className='relative w-full h-full overflow-hidden' aria-label={name}>
       <div
         className='absolute inset-0 bg-contain bg-no-repeat bg-center'
         style={{ backgroundImage: `url(${src})` }}
-      />{' '}
+      />
     </div>
   );
 }
